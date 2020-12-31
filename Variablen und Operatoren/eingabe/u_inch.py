13e0d3ede1ee303bc361632c04745c1e4e3eb49e
Den-Hast-Du-Nicht <cryptonicdev@ctcraft.de>
2020-12-31 16:38:01 +0100

    Übung Eingabe Inch

---
+++ Variablen und Operatoren/eingabe/u_inch.py
@@ -0,0 +1,14 @@
+# Werte
+inch = 2.54
+
+# Eingabe
+print("Bitte geben Sie den Inch-Wert ein:")
+a = input()
+
+# Eingabe in Zahl umwandeln
+b = float(a)
+
+# Berechnung
+c = b * inch
+
+print(b, "Inch sind", c, "cm")
