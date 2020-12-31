24af52d25da4595ecd3a7885002505ccaaeccff3
Den-Hast-Du-Nicht <cryptonicdev@ctcraft.de>
2020-12-31 16:10:52 +0100

    Erstes Programm

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Mein erstes Programm
+print("Hallo Welt")  # Eine Ausgabe
+""" Kommentar in
+    mehreren Zeilen """
+
+# Verkettung von Ausgaben
+print("Hallo", "Welt")
+
+# Lange Ausgaben
+print("Hallo",
+      "Welt")
+print("Hallo ",
+      "Welt")
